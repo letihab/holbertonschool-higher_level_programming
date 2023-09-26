503e4fed06ce1005376dc521e7e06c8fa8916ab1
letihab <letihabib1822@gmail.com>
2023-09-26 13:36:05 +0200

    put string in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for char in str:
+def uppercase(input_str):
+    for char in input_str:
         if ord(char) >= ord('a') and ord(char) <= ord('z'):
             char = chr(ord(char) - ord('a') + ord('A'))
         print('{:s}'.format(char), end='')
