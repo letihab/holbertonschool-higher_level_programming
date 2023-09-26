93517f8da64d1318f4d8097ea996d4f4a54ef782
letihab <letihabib1822@gmail.com>
2023-09-26 11:31:45 +0200

    update file last digit of number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+last_digit = abs(number) % 10  # Obtient la valeur absolue du nombre et calcule le dernier chiffre.
+is_negative = number < 0  # Vérifie si le nombre est négatif.
+
+if is_negative:
+    last_digit = -last_digit  # Change le signe du dernier chiffre si le nombre est négatif.
+
+print("Last digit of", number, "is", last_digit, end=" ")
 
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print("and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0 ")
+    print("and is 0")
 else:
-     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0 ")
+    print("and is less than 6 and not 0")
