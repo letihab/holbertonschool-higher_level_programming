d12a5814a917d1afe75ff03111d345e5db6779dc
letihab <letihabib1822@gmail.com>
2023-09-28 12:57:45 +0200

    hidden file

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    filtered_names = sorted([name for name in names if not name.startswith("__")])
-    for name in filtered_names:
+    namesf = sorted([name for name in names if not name.startswith("__")])
+    for name in namesf:
         print(name)
- 
