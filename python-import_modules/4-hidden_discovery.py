0d1ede1814ca738ca7c08137f03dcbd5718884eb
letihab <letihabib1822@gmail.com>
2023-09-28 12:42:53 +0200

    hidden file

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for i in names:
+    for i in range(len(names)):
         if names[i][0] != '__' and names[i][1] != '__':
             print(names[i])
  
