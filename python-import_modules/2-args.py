1bb6a95050b87eb1f359ca6f880e6cfc7291c507
letihab <letihabib1822@gmail.com>
2023-09-28 11:02:00 +0200

    count argument

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 1:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))
+
