15ded8011780a4e200b3238cb0e80920b2e9dd33
letihab <letihabib1822@gmail.com>
2023-09-28 11:04:16 +0200

    count argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 1:
         print("{} argument:".format(argc))
+    elif argc == 0:
+        print("{} arguments.".format(argc))
     else:
         print("{} arguments:".format(argc))
     for i in range(1, argc + 1):
