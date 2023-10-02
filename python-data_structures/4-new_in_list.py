72eed94a5220f7fe3d7bdcfc10ef7a9921f34532
letihab <letihabib1822@gmail.com>
2023-10-02 02:16:20 +0200

    copy without change the original

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     argc = len(my_list) - 1
-    #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    argc = len(my_list) - 1
     list_copy = my_list.copy()
     if idx < 0:
         return my_list
@@ -12,4 +9,3 @@ def new_in_list(my_list, idx, element):
     else:
         list_copy[idx] = element
     return list_copy
-        
