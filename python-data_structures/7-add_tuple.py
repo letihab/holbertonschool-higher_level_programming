a88e0320f89d8b5b4f4715015ade95c3201114c5
letihab <letihabib1822@gmail.com>
2023-10-02 15:06:04 +0200

    pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     argca = len(tuple_a)
-    argcb = len (tuple_b)
+    argcb = len(tuple_b)
     if argca < 2:
         if argca == 0:
             tuple_a = 0, 0
@@ -12,10 +12,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-
-
-            
-        
-        
+    return (tuple_a[0] + tuple_b[0],
+            tuple_a[1] + tuple_b[1])
