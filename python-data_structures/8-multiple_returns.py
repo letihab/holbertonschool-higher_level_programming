44d07aa13601c9ca36bca2616d3426f14ae5e804
letihab <letihabib1822@gmail.com>
2023-10-02 15:18:23 +0200

    multiples return

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     argc = len(sentence)
     if argc == 0:
-        return (0, None)  
-    else:      
+        return (0, None)
+    else:
         return argc, sentence[0]
