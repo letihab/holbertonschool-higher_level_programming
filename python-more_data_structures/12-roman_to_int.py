3aa5f06777a40a4e7fa0c969fd14fff524cd42ca
letihab <letihabib1822@gmail.com>
2023-10-05 16:44:58 +0200

    roman string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
+def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
     else:
-        dictionary = {'I':1, 'V':5, 'X':10, 'XV':15, 'L':50, 'C':100, 'D':500, 'M':1000}
+        roman_value = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     result = 0
     prev_value = 0
     for number in reversed(roman_string):
-        current_value = dictionary[number]
+        current_value = roman_value.get(number, 0)
         if current_value < prev_value:
             result -= current_value
         else:
