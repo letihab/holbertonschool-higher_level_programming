c66febf486d685fc31918976d2b9ae68f6be27d9
letihab <letihabib1822@gmail.com>
2023-10-05 16:51:59 +0200

    roman string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
     else:
-        roman_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+        roman_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                        'C': 100, 'D': 500, 'M': 1000}
     result = 0
     prev_value = 0
