b7bb6b0a6bc67acae34ccc1a2e5ae53120c26d92
letihab <letihabib1822@gmail.com>
2023-10-06 12:02:23 +0200

    divide 2 integers

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         result = 0
         try:
             result = my_list_1[idx] / my_list_2[idx]
-         except (ZeroDivisionError):
+        except (ZeroDivisionError):
             print('division by 0')
         except (TypeError):
             print('wrong type')
