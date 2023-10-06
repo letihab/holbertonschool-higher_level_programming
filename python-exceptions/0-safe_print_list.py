5e4c7a273f7aceea11f5fbd3fdd0b6bfd91f5df3
letihab <letihabib1822@gmail.com>
2023-10-06 10:52:09 +0200

    print x  elements

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    number_element = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            number_element +=1
+        except IndexError:
+            break
+    print()
+    return number_element
