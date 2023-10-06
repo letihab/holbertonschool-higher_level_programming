a30ba9c5022e780ae62113eec81306663267685c
letihab <letihabib1822@gmail.com>
2023-10-06 11:42:07 +0200

    print and count integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[idx], int):
                 print("{:d}".format(my_list[idx]), end='')
                 printed += 1
-    except (ValueError, TypeError, IndexError):
+    except (ValueError, TypeError):
         pass
     print()
     return (printed)
