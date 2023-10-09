1bcda450d9c9be12890975b703233ee556558078
letihab <letihabib1822@gmail.com>
2023-10-09 12:14:42 +0200

    class square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class definition of square"""
+
+
+class Square:
+    """class square defined"""
+
+    def __init__(self, size):
+        """initialize method"""
+        self.__size = size
