75f9aa896506c0bc825514d83769cb28a7e3b843
letihab <letihabib1822@gmail.com>
2023-10-09 12:42:48 +0200

    size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,12 +5,10 @@
 class Square:
     """class square defined"""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """initialize method"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
-        
