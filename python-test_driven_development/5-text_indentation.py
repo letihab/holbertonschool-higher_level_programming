8aab4607c243d139681a41c37514dad05240b892
letihab <letihabib1822@gmail.com>
2023-10-11 13:05:39 +0200

    indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module text_indentation
+Adds two new lines after a set of characters.
+"""
+
+
+def text_indentation(text):
+    """Prints text with added two newlines
+    after each of these characters {'.', '?', ':'}.
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for delimit in ".:?":
+        text = (delimit + "\n\n").join(
+            [line.strip("") for line in text.split(delimit)])
+
+    print("{}".format(text), end="")
