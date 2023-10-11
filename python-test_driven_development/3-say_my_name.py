17c355fd53256433a988db34149da89d54423d5f
letihab <letihabib1822@gmail.com>
2023-10-11 12:06:11 +0200

    update file txt to print name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,10 +10,9 @@ last_name must be a string
 
 def say_my_name(first_name, last_name=""):
     """function to print my name"""
-    if type(first_name)is  not str:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
-
