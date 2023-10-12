731b36b177b9f5ccf7b76a4e01fa206c295d4649
letihab <letihabib1822@gmail.com>
2023-10-12 11:04:01 +0200

    empty class rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+create an empty class Rectangle,
+that defines a rectangle
+you are not allowed to import any module
+"""
+
+
+class Rectangle:
+    """This is an empty class Rectangle"""
+    pass
