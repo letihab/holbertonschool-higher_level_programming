a855e2e785f61221c23f03e0322b501b3e6b4f09
letihab <letihabib1822@gmail.com>
2023-10-12 11:51:51 +0200

    add property rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     """initialized method of class Rectangle"""
     def __init__(self, width=0, height=0):
         self.__width = width
-        self.__height = height 
+        self.__height = height
 
     @property
     def width(self):
