b918f8bb8770d284eb57c8fd84afd5e58de14a0b
letihab <letihabib1822@gmail.com>
2023-10-12 14:10:46 +0200

    print rectangle with #

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -66,5 +66,6 @@ class Rectangle:
                 for j in range(self.__width):
                     rect += "#"
         return rect
+
     def __repr__(self):
         return f"Rectangle({self.__width},{self.__height})"
