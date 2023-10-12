9ffae92cf3d3e9e9ef938b683070abbe82c4ebf5
letihab <letihabib1822@gmail.com>
2023-10-12 12:28:30 +0200

    return the rectangle perimeter and area

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+create class Rectangle,
+that defines a rectangle
+you are not allowed to import any module
+"""
+
+
+class Rectangle:
+    """initialized method of class Rectangle"""
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        """private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """property setter height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return (" ")
+        string = (('#' * self.__width) + '\n') * self.__height
+        return string[:-1]
