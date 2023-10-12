32aab4678be87691d4a4379078de6fa05871183e
letihab <letihabib1822@gmail.com>
2023-10-12 11:50:14 +0200

    add property rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,9 +7,7 @@ you are not allowed to import any module
 
 
 class Rectangle:
-    """This is an empty class Rectangle"""
-    pass
-
+    """initialized method of class Rectangle"""
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height 
@@ -30,7 +28,7 @@ class Rectangle:
     @property
     def height(self):
         """private instance attribute height"""
-        return self.__width
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -40,5 +38,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-        
