9b955822cd179c0fd49d99e836dce25f2137ce76
letihab <letihabib1822@gmail.com>
2023-10-17 12:15:37 +0200

    check if the code is an instance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+function that return True or False 
+if the object is exactly an instance 
+or not
+"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
