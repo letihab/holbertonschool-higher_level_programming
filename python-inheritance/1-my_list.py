83e90d1b863ecaa10b7d598644246bc48297b84c
letihab <letihabib1822@gmail.com>
2023-10-17 12:02:53 +0200

    add pycodestyle recommandation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,8 +5,8 @@ a class that inherits from list
 
 
 class MyList(list):
-     """class that inherit from list"""
-     
-     def print_sorted(self):
+    """class that inherit from list"""
+
+    def print_sorted(self):
         """function that prints the list, but sorted"""
         print(sorted(self))
