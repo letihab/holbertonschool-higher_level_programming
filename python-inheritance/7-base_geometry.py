f66dbded69fa49841e4ceacf93a0d1b8fa278c28
letihab <letihabib1822@gmail.com>
2023-10-17 14:18:20 +0200

    update file to check if an object is exactly an instance

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""define a base geometry of class Basegeometry"""
+
+
+class BaseGeometry:
+    """empty class BaseGeometry"""
+
+    def area(self):
+        """check if the area is implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
