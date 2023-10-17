e0147bb2748336a89d12f8aaec10ebeb8fe2c31f
letihab <letihabib1822@gmail.com>
2023-10-17 13:06:55 +0200

    update file to check if an object is exactly an instance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+functions that checks if the object is an instance of a class that
+inherited(directly and indirectly) from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """checks if the object is an instance of a class that inherited"""
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
