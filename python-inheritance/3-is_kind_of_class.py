31058e77f0f0b1fdf5b376e2cf955d0d83d0182d
letihab <letihabib1822@gmail.com>
2023-10-17 12:39:14 +0200

    update file to check if an object is exactly an instance

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a function that returns True 
+a function that returns True
 if the object is an instance of,
 or if the object is an instance of
 a class that inherited from, the specified class ; otherwise False.
