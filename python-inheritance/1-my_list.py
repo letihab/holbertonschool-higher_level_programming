525cbbec6f08f35e05d124ae816a638b8035c6d7
letihab <letihabib1822@gmail.com>
2023-10-17 11:21:53 +0200

    list of available attributes

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+a class that inherits from list
+"""
+
+
+class MyList(list):
+     """class that inherit from list"""
+     
+     def print_sorted(self):
+        """function that prints the list, but sorted"""
+        sorted_list = sorted(self)
+        for item in sorted_list:
+            print(item, end=' ')
