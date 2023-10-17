f780aae5808bef7b3b8d7bd51c743f335bad62f7
letihab <letihabib1822@gmail.com>
2023-10-17 13:32:39 +0200

    update file to check if an object is exactly an instance

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""write and empty class"""
+
+
+class BaseGeometry:
+    """empty class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented yet")
