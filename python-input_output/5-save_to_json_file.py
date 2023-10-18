c79c618fd2900cc60ab8d0c901de322ba3070205
letihab <letihabib1822@gmail.com>
2023-10-18 14:53:29 +0200

    save file with json representation of an object

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+function that returns an object
+represented by a JSON string
+"""
+
+
+import json
+
+
+def save_to_json_string(my_obj, filename):
+    """function that returns an object"""
+    with open(filename, "w") as file:
+        json.dump(filename)
