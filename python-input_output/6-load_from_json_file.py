e4767299f295509163f193afd50873cc16d16e6c
letihab <letihabib1822@gmail.com>
2023-10-18 15:10:18 +0200

    save file with json representation of an object

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Write a function that creates
+an Object from a “JSON file”:
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """function that create an object from a json file"""
+    with open(filename) as file:
+        return json.loads(file)
