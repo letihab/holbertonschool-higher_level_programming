8db1e3f96b50c6a11566cdb7b3b0b97d30482be9
letihab <letihabib1822@gmail.com>
2023-10-18 16:08:48 +0200

    define class student with attributes

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""write a class Student that defines a student"""
+
+
+class Student:
+    """class student define"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize a new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dictionnary representation of student"""
+        return self.__dict__
