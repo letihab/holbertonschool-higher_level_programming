7ffd837e37952385c44a5723ff27e19698cd9034
letihab <letihabib1822@gmail.com>
2023-10-18 15:59:12 +0200

    return a dictionary description

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+function that returns the dictionary description with simple
+data structure for json serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """return the dictionnary representation"""
+    return obj.__dict__
