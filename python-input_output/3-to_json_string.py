a7d478b27766ae43959b1a80b30db42c369794d2
letihab <letihabib1822@gmail.com>
2023-10-18 14:21:22 +0200

    json representation of an object

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+a function that returns the JSON representation
+of an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation"""
+    return json.dumps(my_obj)
