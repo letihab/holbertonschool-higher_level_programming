559d684a2c097eaa51e0460d8f277f53a58acc0e
letihab <letihabib1822@gmail.com>
2023-10-18 10:56:59 +0200

    update file to check if an object is exactly an instance

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class that represents a rectangle."""
+
+    def __init__(self, width, height):
+        """initializes width and height"""
+
+        self.__width = width
+        self.integer_validator("width", width)
+        self.__height = height
+        self.integer_validator("height", height)
