bc41e5511ac38df9c5b50068c069f053575921c9
letihab <letihabib1822@gmail.com>
2023-10-18 13:50:26 +0200

    add pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+write a string to a text file
+and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """function to write in the file text"""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
+    
