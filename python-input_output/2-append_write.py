d49e4e26903031ef163d47bce6f1e29442e8829b
letihab <letihabib1822@gmail.com>
2023-10-18 13:59:16 +0200

    append a string to a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Write a function that appends a string at the end
+of a text file (UTF8) and returns the number
+of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """function that append a string at the end of file"""
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
