3a257ccdd42dc402271606c87e9df2718e4b21e3
letihab <letihabib1822@gmail.com>
2023-11-20 14:11:56 +0100

    update file

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 --Script that create table
-CREATE TABLE IF NOT EXISTS first_table ( id  INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table ( `id`  INT, `name` VARCHAR(256));
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 --Get the information_schema
-USE `information_schema`;
-
 SELECT TABLE_NAME, CREATE_TABLE_STATEMENT
 FROM TABLES
 WHERE TABLE_SCHEMA = 'hbtn_0c_0'
