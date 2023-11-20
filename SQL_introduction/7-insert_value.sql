fee866e4e431e78c2f79f02721e6e705af20ffef
letihab <letihabib1822@gmail.com>
2023-11-20 14:40:35 +0100

    insert new row with insert into

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 --Insert a new row in the table on my MYSQL server
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
