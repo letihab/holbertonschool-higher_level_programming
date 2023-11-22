1cae4f4056e21e32984d03d454455bec3d9b7f51
letihab <letihabib1822@gmail.com>
2023-11-22 09:45:27 +0100

    add more recommandation

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,7 +5,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 -- creates a table
 CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    state_id INT NOT NULL
-    name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id)
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES STATES(id)
 );
 
