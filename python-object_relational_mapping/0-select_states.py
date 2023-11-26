4e9c5dccb133663a8c7dd8b732b5ca88e5e34d54
letihab <letihabib1822@gmail.com>
2023-11-26 15:33:26 +0100

    add pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,18 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],  # Change 'connexion' to 'db'
+        db=sys.argv[3],
         port=3306
     )
-    cursor = connection.cursor()
+    curs= db.cursor()
     query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-    states = cursor.fetchall()
+    curs.execute(query)
+    states = curs.fetchall()
     for state in states:
         print(state)
-    cursor.close()
-    connection.close()
+    curs.close()
+    db.close()
