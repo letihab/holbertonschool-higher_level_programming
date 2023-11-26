b4e75e4bf42aaa1f67bd33f0af5d1ed13cf09006
letihab <letihabib1822@gmail.com>
2023-11-26 15:27:51 +0100

    add pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""list all states with a name starting with N from the database hbtn_0e_0_usa"""
+"""list all states with a name starting with
+N from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
@@ -12,16 +13,10 @@ if __name__ == "__main__":
         port=3306
     )
     cursor = connection.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"  # Filter for names starting with 'N'
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     cursor.execute(query)
     states = cursor.fetchall()
     for state in states:
         print(state)
     cursor.close()
     connection.close()
-
-
-
-    
-
-
