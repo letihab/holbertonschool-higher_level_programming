797a1c2d6bcb4a9aa9d2834fd4bd5b76e3144e49
letihab <letihabib1822@gmail.com>
2023-11-27 10:19:41 +0100

    update file

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,23 +4,20 @@ N from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
-def list_states():
-    connexion = MySQLdb.connect(
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306
     )
-    curs = connexion.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC"
+    curs = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     curs.execute(query)
     states = curs.fetchall()
     for state in states:
         print(state)
     curs.close()
-    connexion.close()
-
-
-    if __name__ == "__main__":
-        list_states()
+    db.close()
