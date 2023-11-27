9cec33636702c1b594cebf2401b92523c367176c
letihab <letihabib1822@gmail.com>
2023-11-27 11:16:02 +0100

    filter state by safe injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Displays all values in the states"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
