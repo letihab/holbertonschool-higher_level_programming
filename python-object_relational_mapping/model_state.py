dfb3fc8ae7fc468d5f4187f9d2b0081212e4fb55
letihab <letihabib1822@gmail.com>
2023-11-27 14:34:59 +0100

    add pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from sqlalchemy import Column, Integer, String, MetaData
@@ -8,10 +8,14 @@ from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
     """
     Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    id = Column(
+        Integer, primary_key=True, nullable=False,
+        autoincrement=True, unique=True
+    )
     name = Column(String(128), nullable=False)
