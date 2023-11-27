d0c7e509f7f1099615609126c537c7f778d39bd0
letihab <letihabib1822@gmail.com>
2023-11-27 11:07:41 +0100

    filter state by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        print("Usage: {} username password database state_name"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
