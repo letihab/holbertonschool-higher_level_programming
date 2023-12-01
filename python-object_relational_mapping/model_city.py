ac7dfa546ee746af7c7e03acf0fed94aa5c8efa1
letihab <letihabib1822@gmail.com>
2023-12-01 12:43:47 +0100

    create class city

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'Cities'
+    id = Column(
+        Integer, primary_key=True, nullable=False,
+        autoincrement=True, unique=True
+    )
+    name = Column(String(128), nullable=False)
+    state_id =  Column(Integer, ForeignKey("states.id"), nullable=False)
