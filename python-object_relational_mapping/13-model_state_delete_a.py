acbf947a88147cbda6aefbc457d8a9994d5cc00d
letihab <letihabib1822@gmail.com>
2023-12-01 12:33:21 +0100

    delete state object with names containing a

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""   deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
+"""
+deletes all State objects with a
+name containing the letter a from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
