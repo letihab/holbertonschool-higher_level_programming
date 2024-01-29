cf885304bdfdac10085e12fcccaea2877fe79362
letihab <letihabib1822@gmail.com>
2024-01-29 16:11:45 +0100

    add task 0 to 2

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("#red_header");
-header.addEventListener("click", addclass);
+header.addEventListener("click", addClass);
 
-function addclass() {
-  const redelement = document.querySelector("redelement");
-  redelement.classList.add("red");
+function addClass() {
+  const headerElement = document.querySelector("header");
+  headerElement.classList.add("red");
 }
