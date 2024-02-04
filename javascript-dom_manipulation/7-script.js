53eea2274f86cbc2edc8bdfffbb97f34ce8f10f3
letihab <letihabib1822@gmail.com>
2024-02-04 18:55:48 +0100

    update task 7

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,25 +1,26 @@
-// Wait for the HTML document to be fully loaded before manipulating it
-document.addEventListener("DOMContentLoaded", function () {
-    // Use the Fetch API to fetch the movie data from the URL
-    fetch("https://swapi-api.hbtn.io/api/films/?format=json")
-      .then(response => response.json())
-      .then(movies => showMovieTitles(movies.results)) // Fixed typo in 'results' property
-  
-    function showMovieTitles(movieData) {
-      // Select the HTML ul element with id "list_movies"
-      var listMoviesElement = document.getElementById("list_movies");
-  
-      // Loop through each movie and add its title to the list
-      movieData.forEach(movie => {
-        // Create a new li element
-        var newLiElement = document.createElement("li");
-  
-        // Set the text content of the new li element to the movie title
-        newLiElement.textContent = movie.title;
-  
-        // Append the new li element to the ul
-        listMoviesElement.appendChild(newLiElement);
-      });
+const liste = document.querySelector('#list_movies');
+
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Erreur réseau');
     }
+    return response.json(); // Traiter la réponse JSON ici
+  })
+  .then((data) => {
+    const { results } = data;
+    for (const movie of results) {
+      const { title } = movie;
+
+      const newLi = document.createElement('li');
+      const newText = document.createTextNode(title);
+      newLi.appendChild(newText);
+      liste.appendChild(newLi);
+    }
+  })
+  .catch((error) => {
+    const newLi = document.createElement('li');
+    const newText = document.createTextNode(error.message);
+    newLi.appendChild(newText);
+    liste.appendChild(newLi);
   });
-  
